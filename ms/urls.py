0b8081ee624291516feb0bede916651ecb819440
edcilo <edcilop@gmail.com>
2021-09-28 17:09:35 -0500

    test error

--- ms/urls.py
+++ ms/urls.py
@@ -6,11 +6,4 @@ from ms.counter import counter
 @app.route('/')
 def index():
     counter.sum()
-    return jsonify({
-        "data": {
-            "name": "Flask01",
-            "requets": counter.c,
-            "version": "1.0.1",
-        },
-        "code": 200
-    }), 200
+    return "error :(", 200
