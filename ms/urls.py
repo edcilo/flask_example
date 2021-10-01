6961c7d35f33304f9536ea10903a9239447b0664
edcilo <edcilop@gmail.com>
2021-10-01 14:30:29 -0500

    fix error

--- ms/urls.py
+++ ms/urls.py
@@ -6,12 +6,11 @@ from ms.counter import counter
 @app.route('/')
 def index():
     counter.sum()
-    # return jsonify({
-    #     "data": {
-    #         "name": "Flask01",
-    #         "requets": counter.c,
-    #         "version": "1.0.2",
-    #     },
-    #     "code": 200
-    # }), 200
-    return "hello world", 200
+    return jsonify({
+        "data": {
+            "name": "Flask01",
+            "requets": counter.c,
+            "version": "1.0.2",
+        },
+        "code": 200
+    }), 200
